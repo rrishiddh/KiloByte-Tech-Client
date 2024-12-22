2c1159893ec6c7aa88013dd01c8427e94604fc2e
rrishiddh <rrishiddh@gmail.com>
2024-12-22 22:27:27 +0600

    addBlogs page added

--- src/components/AddBlog.jsx
+++ src/components/AddBlog.jsx
@@ -1,10 +1,158 @@
+import { useContext, useState } from "react";
+import { AuthContext } from "./AuthProvider";
+import Swal from "sweetalert2";
 
 const AddBlog = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+    const { user } = useContext(AuthContext);
+  const [formData, setFormData] = useState({
+    title: "",
+    imageUrl: "",
+    category: "",
+    longDescription: "",
+    shortDescription: "",
+    postingDate: "",
+  });
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const currentDateTime = new Date().toLocaleString();
+
+    const addToBlog = {
+      ...formData,
+      postingDate: currentDateTime,
+      userEmail: user.email,
+      userName: user.displayName,
+    };
+
+    fetch("http://localhost:5000/allBlog", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(addToBlog),
+    })
+      .then((res) => res.json())
+      .then(() => {
+        Swal.fire({
+          title: "Blog Post Submitted Successfully!",
+          icon: "success",
+        });
+        setFormData({
+            title: "",
+            imageUrl: "",
+            category: "",
+            longDescription: "",
+            shortDescription: "",
+            postingDate: "",
+        });
+      });
+  };
+
+  return (
+    <>
+      <div className="min-h-screen justify-center items-center">
+        <h2 className="text-2xl font-semibold pt-2 text-center"> Add New Blog Post!</h2>
+        <p className="font-light text-center py-2 mb-6">
+          Share What New Is Happening In The Industry!
+        </p>
+        <div className="flex justify-center items-center">
+          <div className="card  bg-base-200 mb-6 border-2 md:w-full max-w-[80%] shrink-0 p-10">
+              <form onSubmit={handleSubmit} className="card-body">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                  {/* Title */}
+                  <div className="form-control">
+                    <label className="label">
+                      <span className="label-text">Blog Title</span>
+                    </label>
+                    <input
+                      name="title"
+                      type="text"
+                      placeholder="title"
+                      className="input input-bordered"
+                      value={formData.title}
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+
+                  {/* image */}
+                  <div className="form-control">
+                    <label className="label">
+                      <span className="label-text">Post Thumbnail</span>
+                    </label>
+                    <input
+                      name="imageUrl"
+                      type="text"
+                      placeholder="thumbnail-URL"
+                      className="input input-bordered"
+                      value={formData.imageUrl}
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+
+                  {/* shortDescription */}
+                  <div className="form-control">
+                    <label className="label">
+                      <span className="label-text">Short Description</span>
+                    </label>
+                    <input
+                      name="shortDescription"
+                      placeholder="add only 10 words"
+                      className="textarea textarea-bordered"
+                      value={formData.shortDescription}
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+
+                    {/* category */}
+                    <div className="form-control">
+                    <label className="label">
+                      <span className="label-text">Category</span>
+                    </label>
+                    <select
+                      name="category"
+                      className="select select-bordered"
+                      value={formData.category}
+                      onChange={handleChange}
+                      defaultValue={'Cybersecurity'}
+                    >
+                      <option value="Cybersecurity">Cybersecurity</option>
+                      <option value="ArtificialIntelligence">Artificial Intelligence</option>
+                      <option value="WebDevelopment">Web Development</option>
+                      <option value="SoftwareDevelopment">Software Development</option>
+                      <option value="GeneralTech">General Tech</option>
+                    </select>
+                  </div>
+
+                  {/* longDescription */}
+                  <div className="form-control md:col-span-2">
+                    <label className="label">
+                      <span className="label-text">Long Description</span>
+                    </label>
+                    <textarea name="longDescription"
+                      placeholder="write in details..."                 
+                      className="textarea textarea-bordered "
+                      value={formData.longDescription}
+                      onChange={handleChange}
+                      required></textarea>
+                  </div>
+                 
+                </div>
+                <div className="form-control w-[50%] mx-auto mt-6">
+                  <button type="submit" className="btn btn-primary">
+                    Submit
+                  </button>
+                </div>
+              </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 export default AddBlog;
